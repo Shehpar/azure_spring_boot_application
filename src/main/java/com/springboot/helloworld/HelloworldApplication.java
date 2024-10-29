038672484f0648352da75d0a6e9cc05a3b7d0002
Shehpar Hassan <96947844+Shehpar@users.noreply.github.com>
2024-10-29 15:13:14 +0500

    Update HelloworldApplication.java

--- src/main/java/com/springboot/helloworld/HelloworldApplication.java
+++ src/main/java/com/springboot/helloworld/HelloworldApplication.java
@@ -1,21 +1,13 @@
-package com.javatechie.azure.demo;
+package com.springboot.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class SpringAzureDemoApplication {
-
-	@GetMapping("/message")
-	public String message(){
-		return "live from Azure";
-	}
+public class HelloworldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAzureDemoApplication.class, args);
+		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
 }
